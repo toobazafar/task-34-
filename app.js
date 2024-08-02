518ab30e48126ac7058cbab29eec1e75ce3f190e
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-08-02 22:32:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var myPizza = ["Cheeze Pizza", "Pepperoni Pizza", "Vegeterian Pizza"];
+// print only names of pizza
+for (var i = 0; i < myPizza.length; i++) {
+    console.log(myPizza[i]);
+}
---
+++ app.ts
@@ -0,0 +1,6 @@
+let myPizza = ["Cheeze Pizza", "Pepperoni Pizza", "Vegeterian Pizza"];
+
+// print only names of pizza
+for (let i = 0; i < myPizza.length; i++) {
+console.log(myPizza[i]);
+}
